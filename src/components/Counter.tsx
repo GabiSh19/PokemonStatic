d4238e55e85d4b7f3f23eb176a5956e133d2ca04
Gabi13st.h <gabs13.h@gmail.com>
2024-06-30 22:57:09 -0400

    añade ruta a islas

---
+++ src/components/Counter.tsx
@@ -0,0 +1,28 @@
+import { createSignal } from "solid-js"
+
+
+export const Counter = () => {
+
+    const [ counter, setCounter ] = createSignal(10);
+
+
+    return (
+        <>
+            <h1 class = 'text-4xl'>Counter</h1>
+            <h1 class = 'text-xl'>Value: { counter() }</h1>
+        
+            <button 
+            onclick={ () => setCounter((prev) => --prev) }
+            class= "bg-blue-500 p-2 mr-2 rounded" >-1</button>
+
+            
+            <button 
+            onclick={ () => setCounter((prev) => ++prev) }
+            class= "bg-blue-500 p-2 mr-2 rounded" >+1</button>
+
+
+        </>
+
+    )
+
+}
