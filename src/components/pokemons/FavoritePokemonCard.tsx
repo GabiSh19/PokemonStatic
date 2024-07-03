647822f0b4b84a624dea414dda7c005ad2daf388
Gabi13st.h <gabs13.h@gmail.com>
2024-07-03 12:17:09 -0400

    agrega transition a favoritepage

--- src/components/pokemons/FavoritePokemonCard.tsx
+++ src/components/pokemons/FavoritePokemonCard.tsx
@@ -1,5 +1,5 @@
 import type { FavoritePokemon } from "@interfaces/favorite-pokemon";
-import { createSignal, type Component } from "solid-js";
+import { createSignal, Show, type Component } from "solid-js";
 
 interface Props {
     pokemon: FavoritePokemon,
@@ -11,19 +11,39 @@ export const FavoritePokemonCard: Component <Props>  = ({pokemon}) => {
     const [ isVisible, setIsVisible ]= createSignal(true);    
 
     const imageSrc = ` https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon.id}.png`
+    const deleteFavorite = () => {
+
+        const favorites = JSON.parse(
+            localStorage.getItem('favorites') ?? '[]'
+        ) as FavoritePokemon[];
+
+        const newFavorites = favorites.filter(p => p.id !== pokemon.id);
+
+        localStorage.setItem('favorites', JSON.stringify(newFavorites));
+        setIsVisible(false);
+    }
 
     return (
-        <div class="flex flex-col justify-center items-center">
-            <a href={`/pokemon/${pokemon.name}`}>
-                <img src={imageSrc} alt={pokemon.name}
-                    width="96" height="96" />
-                <p class="capitalize">
-                    #{pokemon.id} {pokemon.name}
-                </p>
-            </a>
-            <button class="text-red-400">Borrar</button>
-        </div>
-
+        <Show when= {isVisible()}>
+            <div class="flex flex-col justify-center items-center">
+                <a href={`/pokemons/${pokemon.name}`}>
+                    <img 
+                        src={imageSrc} 
+                        alt={pokemon.name}
+                        width="96" 
+                        height="96" 
+                        style = {`view-transition-name: ${pokemon.name}-image`}
+                        />
+                        
+                    <p class="capitalize">
+                        #{pokemon.id} {pokemon.name}
+                    </p>
+                </a>
+                <button 
+                onclick={deleteFavorite}
+                class="text-red-400">Borrar</button>
+            </div>
+        </Show>
 
     )
 }
